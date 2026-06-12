04799b744e08dc813b2415e23199b885fb0fd954
Michael S Muegel <mike@muegel.org>
2026-06-12 13:00:55 -0500

    Fix for Sundial Classic

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "netswift",
     platforms: [
-        .watchOS("9.0")
+        .watchOS("7.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
